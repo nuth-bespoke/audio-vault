471f58f8125795ae1170691a81bb12ad2a543d8e
Paul Bradley <paul.bradley4@nhs.net>
2025-02-17 12:33:11 +0000

    modified db schema

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 db:
+	rm ./database/audio-vault.*
 	cat ./database/schema.sql | sqlite3 ./database/audio-vault.db
 
--- database/schema.sql
+++ database/schema.sql
@@ -4,13 +4,29 @@
 PRAGMA user_version = 1; -- https://www.sqlite.org/pragma.html#pragma_user_version
 PRAGMA journal_mode = WAL; -- https://www.sqlite.org/pragma.html#pragma_journal_mode
 
-DROP TABLE IF EXISTS Dictation;
+DROP TABLE IF EXISTS Dictations;
 
-CREATE TABLE Dictation (
-    DocumentID          INTEGER PRIMARY KEY,
-    MRN                 TEXT NOT NULL,
-    DocumentName        TEXT NOT NULL DEFAULT '?'
+CREATE TABLE Dictations (
+    DocumentID      INTEGER PRIMARY KEY,
+    MRN             TEXT NOT NULL,
+    DocumentName    TEXT NOT NULL DEFAULT '?',
+    CreatedBy       TEXT NOT NULL,
+    MachineName     TEXT NOT NULL,
+    SavedAt         TEXT NOT NULL
 );
 
-CREATE INDEX idx_valid_mrn ON Dictation (MRN);
+CREATE INDEX idx_dictation_mrn ON Dictations (MRN);
+
+
+DROP TABLE IF EXISTS Segments;
+
+CREATE TABLE Segments (
+    SegmentFileName     TEXT NOT NULL PRIMARY KEY,
+    SegmentFileSize     INTEGER NOT NULL,
+    SegmentFileOrder    INTEGER NOT NULL,
+    AudioBitRate        TEXT NOT NULL,
+    AudioDuration       TEXT NOT NULL,
+    AudioPrecision      TEXT NOT NULL,
+    AudioSampleRate     TEXT NOT NULL
+);
 
